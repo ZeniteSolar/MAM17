2d27ae6e6c2e834fee4d540f5458302b54d88294
João Antônio Cardoso <joao.maker@gmail.com>
2017-10-22 17:11:08 -0200

    Added IDS for MIC17 and assigned to receive MIC17 STATE and MOTOR msgs.

--- firmware/src/can_filters.h
+++ firmware/src/can_filters.h
@@ -58,17 +58,17 @@
 const uint8_t can_filter[] PROGMEM =
 {
     // Group 0
-    MCP2515_FILTER(CAN_FILTER_MSG_MIC17_STATE),              // Filter 0
-    MCP2515_FILTER(CAN_FILTER_MSG_MIC17_MOTOR),              // Filter 1
+    MCP2515_FILTER(CAN_FILTER_MSG_MIC17_STATE), // Filter 0
+    MCP2515_FILTER(CAN_FILTER_MSG_MIC17_MOTOR), // Filter 1
 
     // Group 1
-    MCP2515_FILTER(CAN_FILTER_IGNOREALL),     // Filter 2
-    MCP2515_FILTER(CAN_FILTER_IGNOREALL),     // Filter 3
-    MCP2515_FILTER(CAN_FILTER_IGNOREALL),     // Filter 4
-    MCP2515_FILTER(CAN_FILTER_IGNOREALL),     // Filter 5
+    MCP2515_FILTER(CAN_FILTER_MSG_MIC17_STATE), // Filter 2
+    MCP2515_FILTER(CAN_FILTER_MSG_MIC17_MOTOR), // Filter 3
+    MCP2515_FILTER(CAN_FILTER_MSG_MIC17_STATE), // Filter 4
+    MCP2515_FILTER(CAN_FILTER_MSG_MIC17_MOTOR), // Filter 5
 
-    MCP2515_FILTER(CAN_FILTER_IGNOREALL),              // Mask 0 (for group 0)
-    MCP2515_FILTER(CAN_FILTER_IGNOREALL),     // Mask 1 (for group 1)
+    MCP2515_FILTER(CAN_MASK_MIC17),             // Mask 0 (for group 0)
+    MCP2515_FILTER(CAN_MASK_MIC17),             // Mask 1 (for group 1)
 };
 // You can receive 11 bit identifiers with either group 0 or 1.
 
--- firmware/src/can_ids.h
+++ firmware/src/can_ids.h
@@ -1,8 +1,8 @@
 #ifndef _CAN_IDS_H_
 #define _CAN_IDS_H_ 
 
-#define CAN_FILTER_ACCEPTALL            0       //<! accept all messages
-#define CAN_FILTER_IGNOREALL            999999  //<! ignore all messages
+#define CAN_FILTER_ACCEPTALL            0b00000000000  //<! accept all messages
+#define CAN_FILTER_IGNOREALL            0b11111111111  //<! ignore all messages
  
 
 // GENERIC FIELDS FROM ANY MODULE's STATE MESSAGE
@@ -12,17 +12,18 @@
 
 
 // MESSGES FROM MIC17:
-#define CAN_FILTER_MSG_MIC17_STATE      7       //<! Informs about state and errors
-#define CAN_FILTER_MSG_MIC17_MOTOR      9       //<! Informs about motor controls.
-#define CAN_FILTER_MSG_MIC17_PUMPS      11      //<! Informs about pumps controls.
-#define CAN_FILTER_MSG_MIC17_MPPTS      10      //<! Informs about mppts controls.
+#define CAN_MASK_MIC17                  0b00000100011   //<! Mask for MIC17
+#define CAN_FILTER_MSG_MIC17_STATE      0b00000100000   //<! Informs about state and errors
+#define CAN_FILTER_MSG_MIC17_MOTOR      0b00000100001   //<! Informs about motor controls.
+#define CAN_FILTER_MSG_MIC17_PUMPS      0b00000100010   //<! Informs about pumps controls.
+#define CAN_FILTER_MSG_MIC17_MPPTS      0b00000100011   //<! Informs about mppts controls.
 
 // MESSAGES FROM AM17:
-#define CAN_FILTER_MSG_AM17_STATE       17      //<! Informs about state and errors
-#define CAN_FILTER_MSG_AM17_MOTOR       18      //<! Informs about motor
+//#define CAN_FILTER_MSG_AM17_STATE       0b00000000000    //<! Informs about state and errors
+//#define CAN_FILTER_MSG_AM17_MOTOR       0b00000000000      //<! Informs about motor
 
 // MESSAGES FROM MAB17
-#define CAN_FILTER_MSG_MAB17_STATE      276     //<! Informs about state and errors
-#define CAN_FILTER_MSG_MAB17_PUMP       277     //<! Informs about motor
+//#define CAN_FILTER_MSG_MAB17_STATE      0b00000000000     //<! Informs about state and errors
+//#define CAN_FILTER_MSG_MAB17_PUMP       0b00000000000    //<! Informs about motor
 
 #endif /* ifndef _CAN_IDS_H_ */
