cef49f77d36213e4478c7eb3ea35f8dde0419994
João Antônio Cardoso <joao.maker@gmail.com>
2017-10-29 19:41:26 -0200

    Fixed some verbose messages.

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -6,46 +6,46 @@ int main(void)
 {
     #ifdef USART_ON
         usart_init(MYUBRR,1,1);                         // inicializa a usart
-        VERBOSE0_MSG(usart_send_string("\n\n\nUSART... OK!\n"));
+        VERBOSE_MSG_INIT(usart_send_string("\n\n\nUSART... OK!\n"));
     #endif
 
     #ifdef CAN_ON
-        VERBOSE0_MSG(usart_send_string("CAN (125kbps)..."));
+        VERBOSE_MSG_INIT(usart_send_string("CAN (125kbps)..."));
         can_init(BITRATE_125_KBPS);
-        VERBOSE0_MSG(usart_send_string(" OK!\n"));
-        VERBOSE0_MSG(usart_send_string("CAN filters..."));
+        VERBOSE_MSG_INIT(usart_send_string(" OK!\n"));
+        VERBOSE_MSG_INIT(usart_send_string("CAN filters..."));
         can_static_filter(can_filter);
-        VERBOSE0_MSG(usart_send_string(" OK!\n"));
+        VERBOSE_MSG_INIT(usart_send_string(" OK!\n"));
     #endif
 
     #ifdef ADC_ON
-        VERBOSE0_MSG(usart_send_string("ADC..."));
+        VERBOSE_MSG_INIT(usart_send_string("ADC..."));
         adc_init();
-        VERBOSE0_MSG(usart_send_string(" OK!\n"));
+        VERBOSE_MSG_INIT(usart_send_string(" OK!\n"));
     #endif
 
     #ifdef PWM_ON
-        VERBOSE0_MSG(usart_send_string("PWM..."));
+        VERBOSE_MSG_INIT(usart_send_string("PWM..."));
         pwm_init();
-        VERBOSE0_MSG(usart_send_string(" OK!\n"));
+        VERBOSE_MSG_INIT(usart_send_string(" OK!\n"));
     #endif 
 
     #ifdef SLEEP_ON 
-        VERBOSE0_MSG(usart_send_string("SLEEP..."));
+        VERBOSE_MSG_INIT(usart_send_string("SLEEP..."));
         sleep_init();
-        VERBOSE0_MSG(usart_send_string(" OK!\n"));
+        VERBOSE_MSG_INIT(usart_send_string(" OK!\n"));
     #endif
 
     #ifdef WATCHDOG_ON
-        VERBOSE0_MSG(usart_send_string("WATCHDOG..."));
+        VERBOSE_MSG_INIT(usart_send_string("WATCHDOG..."));
         wdt_init();
-        VERBOSE0_MSG(usart_send_string(" OK!\n"));
+        VERBOSE_MSG_INIT(usart_send_string(" OK!\n"));
     #endif
 
  	#ifdef MACHINE_ON
-        VERBOSE0_MSG(usart_send_string("MACHINE..."));
+        VERBOSE_MSG_INIT(usart_send_string("MACHINE..."));
 		machine_init();
-        VERBOSE0_MSG(usart_send_string(" OK!\n"));
+        VERBOSE_MSG_INIT(usart_send_string(" OK!\n"));
 	#endif
 
     sei();
@@ -72,9 +72,9 @@ int main(void)
 ISR(BADISR_vect)
 {
     for(;;){
-        VERBOSE0_MSG(usart_send_string("\nFATAL ERROR: BAD ISR."));
+        VERBOSE_MSG_ERROR(usart_send_string("\nFATAL ERROR: BAD ISR."));
         #ifdef WATCHDOG_ON
-            VERBOSE0_MSG(usart_send_string("WAITING FOR WATCHDOG TO RESET...\n"));
+            VERBOSE_MSG_ERROR(usart_send_string("WAITING FOR WATCHDOG TO RESET...\n"));
         #endif
         #ifdef DEBUG_ON
             DEBUG0;
