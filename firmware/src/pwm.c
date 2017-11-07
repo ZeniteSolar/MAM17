57f0a44e5ff5607cd7b14afb9de020442d74600d
João Antônio Cardoso <joao.maker@gmail.com>
2017-11-07 17:19:33 -0200

    PWM arrumado.

--- firmware/src/pwm.c
+++ firmware/src/pwm.c
@@ -1,4 +1,5 @@
 #include "pwm.h"
+#include <util/delay.h>
 
 /**
  * @brief configura o PWM usando o timer TC1
@@ -6,17 +7,28 @@
 void pwm_init()
 {
     // configuracao do Timer TC1 --> TIMER DO PWM
-    TCCR1B |= ((0<<ICNC1) | (0<<ICES1));
-    TCCR1B |= ((1<<WGM13) | (0<<WGM12));
-    TCCR1B |= ((0<<CS12) | (0<<CS11)); 
-    TCCR1B |=  (1<<CS10);
-    TCCR1A |= ((0<<COM1A1) | (1<<COM1A0));
-    TCCR1A |= ((0<<COM1B1) | (1<<COM1B0)); 
+    //TCCR1B |= ((0<<ICNC1) | (0<<ICES1));
+    
+    /*TCCR1A = TCCR1B = 0;
+    
+    TCCR1B |= ((0<<CS12) | (0<<CS11) | (1<<CS10));
+    TCCR1A |= ((1<<COM1A1) |    (0<<COM1A0));
+    TCCR1A |= ((0<<COM1B1) |    (0<<COM1B0)); 
     TCCR1A |= ((1<<WGM11) | (0<<WGM10));
+    TCCR1B |= ((0<<WGM13) | (0<<WGM12));
+    
+    */
+  	TCCR1A |= 0b10000010;
+  	TCCR1B |= 0b00010001;
 
+    set_bit(PWM_DDR, PWM);                          // PWM como saida
+
+    ICR1   = 640;                                   // valor TOP para f_pwm = 12 KHz
+    OCR1A  = 320;//INITIAL_D;                             // D = %*ICR1
+      
     // Equacao para Frequencia do PWM:       ICR1 = (f_osc)/(2*f_pwm);
-	ICR1   = 640;                                   // valor TOP para f_pwm = 12 KHz
-	OCR1A  = INITIAL_D;                             // D = %*ICR1
+
+
 }
 
 /**
@@ -52,9 +64,13 @@ inline void pwm_compute(void)
     if(control.D > PWM_D_MAX_THRESHHOLD)        control.D = PWM_D_MAX;
     else if(control.D < PWM_D_MIN_THRESHHOLD)   control.D = PWM_D_MIN;
 
+    // apply dutycycle
+    OCR1A = control.D;
+
     VERBOSE_MSG_PWM(usart_send_string("PWM computed as: "));
-    VERBOSE_MSG_PWM(usart_send_uint16(control.D));
+    VERBOSE_MSG_PWM(usart_send_uint16(OCR1A));
     VERBOSE_MSG_PWM(usart_send_char('\n'));
+ 
 }
 
 /**
