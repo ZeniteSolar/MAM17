29fb85687e2d6ed42af99a12ddf4a160517bc945
João Antônio Cardoso <joao.maker@gmail.com>
2020-01-19 23:01:44 -0300

    avr-can-lib updated

---
+++ firmware/lib/avr-can-lib/src/.dep/mcp2515_regdump.o.d
@@ -0,0 +1,16 @@
+build/mcp2515_regdump.o: mcp2515_regdump.c mcp2515_private.h ../can.h \
+ config.h utils.h can_private.h spi.h mcp2515_defs.h
+
+mcp2515_private.h:
+
+../can.h:
+
+config.h:
+
+utils.h:
+
+can_private.h:
+
+spi.h:
+
+mcp2515_defs.h:
---
+++ firmware/lib/avr-can-lib/src/.dep/spi.o.d
@@ -0,0 +1,16 @@
+build/spi.o: spi.c mcp2515_private.h ../can.h config.h utils.h \
+ can_private.h spi.h mcp2515_defs.h
+
+mcp2515_private.h:
+
+../can.h:
+
+config.h:
+
+utils.h:
+
+can_private.h:
+
+spi.h:
+
+mcp2515_defs.h:
