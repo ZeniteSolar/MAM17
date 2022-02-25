dcbd1abb6c2b0b30c704f5fd0401097ffd2afa65
João Antônio Cardoso <joao.maker@gmail.com>
2022-02-25 16:54:48 -0300

    Add extractor for Contactor messages, and error checking.

--- firmware/src/can_app.c
+++ firmware/src/can_app.c
@@ -3,6 +3,7 @@
 uint8_t can_app_send_state_clk_div;
 uint8_t can_app_send_motor_clk_div;
 uint16_t can_app_checks_without_mic19_msg;
+uint16_t can_app_checks_without_mac22_msg;
 uint16_t can_app_checks_without_mswi19_msg;
 uint8_t mswi19_connected;
 
@@ -97,12 +98,45 @@ inline void can_app_send_contactor_request(uint8_t request)
 }
 
 /**
+ * @brief extracts the specific MIC19 MOTOR  message
+ *
+ * The msg is AAAAAAAABBBBBBBB
+ * A is the Signature of module
+ * B is the Response
+ *
+ * @param *msg pointer to the message to be extracted
+*/
+inline void can_app_extractor_mac22_contactor_response(can_t *msg)
+{
+    if(msg->data[CAN_MSG_GENERIC_STATE_SIGNATURE_BYTE] == CAN_SIGNATURE_MAC22){
+        can_app_checks_without_mac22_msg = 0;
+
+        contactor.message_received        = msg->data[CAN_MSG_MAC22_CONTACTOR_RESPONSE_BYTE];
+    }
+}
+
+/**
  * @brief extracts the specific MIC19 STATE message
  * @param *msg pointer to the message to be extracted
  */
 inline void can_app_extractor_mic19_state(can_t *msg)
 {
     if(msg->data[CAN_MSG_GENERIC_STATE_SIGNATURE_BYTE] == CAN_SIGNATURE_MIC19){
+        // zerar contador
+        if(msg->data[CAN_MSG_GENERIC_STATE_ERROR_BYTE]){
+            //ERROR!!!
+        }
+        /*if(contador == maximo)*/{
+            //ERROR!!!
+        }
+
+
+    }
+}
+
+inline void can_app_extractor_mac22_state(can_t *msg)
+{
+    if(msg->data[CAN_MSG_GENERIC_STATE_SIGNATURE_BYTE] == CAN_SIGNATURE_MAC22){
         // zerar contador
         if(msg->data[CAN_MSG_GENERIC_STATE_ERROR_BYTE]){
             //ERROR!!!
@@ -163,37 +197,76 @@ inline void can_app_extractor_mic19_motor(can_t *msg)
         }
 
         control.I_raw_target        = msg->data[CAN_MSG_MIC19_MOTOR_I_BYTE];
-
-    }
-    if(msg->data[CAN_MSG_GENERIC_STATE_SIGNATURE_BYTE] == CAN_SIGNATURE_MSWI19){
-        control.D_raw_target        = msg->data[CAN_MSG_MIC19_MOTOR_D_BYTE];
     }
 }
 
 inline void can_app_extractor_mswi19_motor(can_t *msg)
 {
     if(msg->data[CAN_MSG_GENERIC_STATE_SIGNATURE_BYTE] == CAN_SIGNATURE_MSWI19){
-            can_app_checks_without_mswi19_msg = 0;
-            mswi19_connected = 1;
-        /*
-        system_flags.motor_on       = bit_is_set(msg->data[
-            CAN_MSG_MSWI19_MOTOR_MOTOR_BYTE],
-            CAN_MSG_MSWI19_MOTOR_MOTOR_MOTOR_ON_BIT);
-
-        system_flags.dms            = bit_is_set(msg->data[
-            CAN_MSG_MSWI19_MOTOR_MOTOR_BYTE],
-            CAN_MSG_MSWI19_MOTOR_MOTOR_DMS_ON_BIT);
-        */
+        can_app_checks_without_mswi19_msg = 0;
+        mswi19_connected = 1;
 
         control.D_raw_target    = msg->data[CAN_MSG_MSWI19_MOTOR_D_BYTE];
-
-        /*
-        control.I_raw_target        = msg->data[CAN_MSG_MSWI19_MOTOR_I_BYTE];
-        */
-
     }
-    if(msg->data[CAN_MSG_GENERIC_STATE_SIGNATURE_BYTE] == CAN_SIGNATURE_MSWI19){
-        control.D_raw_target        = msg->data[CAN_MSG_MSWI19_MOTOR_D_BYTE];
+}
+
+inline void can_app_msg_mic19_extractors_switch(can_t *msg)
+{
+    switch(msg->id){
+        case CAN_MSG_MIC19_MOTOR_ID:
+            VERBOSE_MSG_CAN_APP(usart_send_string("got a motor msg from mic19: "));
+            VERBOSE_MSG_CAN_APP(can_app_print_msg(msg));
+            can_app_extractor_mic19_motor(msg);
+            break;
+        case CAN_MSG_MIC19_STATE_ID:
+            VERBOSE_MSG_CAN_APP(usart_send_string("got a state msg from mic19: "));
+            VERBOSE_MSG_CAN_APP(can_app_print_msg(msg));
+            can_app_extractor_mic19_state(msg);
+            break;
+        default:
+            VERBOSE_MSG_CAN_APP(usart_send_string("got a unknown msg from mic19: "));
+            VERBOSE_MSG_CAN_APP(can_app_print_msg(msg));
+            break;
+    }
+}
+
+inline void can_app_msg_mswi19_extractors_switch(can_t *msg)
+{
+    switch(msg->id){
+        case CAN_MSG_MSWI19_MOTOR_ID:
+            VERBOSE_MSG_CAN_APP(usart_send_string("got a motor msg from mswi19: "));
+            VERBOSE_MSG_CAN_APP(can_app_print_msg(msg));
+            can_app_extractor_mswi19_motor(msg);
+            break;
+        case CAN_MSG_MSWI19_STATE_ID:
+            VERBOSE_MSG_CAN_APP(usart_send_string("got a state msg from mswi19: "));
+            VERBOSE_MSG_CAN_APP(can_app_print_msg(msg));
+            can_app_extractor_mswi19_state(msg);
+            break;
+        default:
+            VERBOSE_MSG_CAN_APP(usart_send_string("got a unknown msg from mswi19: "));
+            VERBOSE_MSG_CAN_APP(can_app_print_msg(msg));
+            break;
+    }
+}
+
+inline void can_app_msg_mac22_extractors_switch(can_t *msg)
+{
+    switch(msg->id){
+        case CAN_MSG_MAC22_CONTACTOR_ID:
+            VERBOSE_MSG_CAN_APP(usart_send_string("got a motor msg from mac22: "));
+            VERBOSE_MSG_CAN_APP(can_app_print_msg(msg));
+            can_app_extractor_mac22_contactor_response(msg);
+            break;
+        case CAN_MSG_MAC22_STATE_ID:
+            VERBOSE_MSG_CAN_APP(usart_send_string("got a state msg from mac22: "));
+            VERBOSE_MSG_CAN_APP(can_app_print_msg(msg));
+            can_app_extractor_mac22_state(msg);
+            break;
+        default:
+            VERBOSE_MSG_CAN_APP(usart_send_string("got a unknown msg from mac22: "));
+            VERBOSE_MSG_CAN_APP(can_app_print_msg(msg));
+            break;
     }
 }
 
@@ -203,41 +276,17 @@ inline void can_app_extractor_mswi19_motor(can_t *msg)
  */
 inline void can_app_msg_extractors_switch(can_t *msg)
 {
-    if(msg->data[CAN_MSG_GENERIC_STATE_SIGNATURE_BYTE] == CAN_SIGNATURE_MIC19){
-        switch(msg->id){
-            case CAN_MSG_MIC19_MOTOR_ID:
-                VERBOSE_MSG_CAN_APP(usart_send_string("got a motor msg from mic19: "));
-                VERBOSE_MSG_CAN_APP(can_app_print_msg(msg));
-                can_app_extractor_mic19_motor(msg);
-                break;
-            case CAN_MSG_MIC19_STATE_ID:
-                VERBOSE_MSG_CAN_APP(usart_send_string("got a state msg from mic19: "));
-                VERBOSE_MSG_CAN_APP(can_app_print_msg(msg));
-                can_app_extractor_mic19_state(msg);
-                break;
-            default:
-                VERBOSE_MSG_CAN_APP(usart_send_string("got a unknown msg from mic19: "));
-                VERBOSE_MSG_CAN_APP(can_app_print_msg(msg));
-                break;
-        }
-    }
-    if(msg->data[CAN_MSG_GENERIC_STATE_SIGNATURE_BYTE] == CAN_SIGNATURE_MSWI19){
-        switch(msg->id){
-            case CAN_MSG_MSWI19_MOTOR_ID:
-                VERBOSE_MSG_CAN_APP(usart_send_string("got a motor msg from mswi19: "));
-                VERBOSE_MSG_CAN_APP(can_app_print_msg(msg));
-                can_app_extractor_mswi19_motor(msg);
-                break;
-            case CAN_MSG_MSWI19_STATE_ID:
-                VERBOSE_MSG_CAN_APP(usart_send_string("got a state msg from mswi19: "));
-                VERBOSE_MSG_CAN_APP(can_app_print_msg(msg));
-                can_app_extractor_mswi19_state(msg);
-                break;
-            default:
-                VERBOSE_MSG_CAN_APP(usart_send_string("got a unknown msg from mswi19: "));
-                VERBOSE_MSG_CAN_APP(can_app_print_msg(msg));
-                break;
-        }
+    switch(msg->data[CAN_MSG_GENERIC_STATE_SIGNATURE_BYTE]){
+        case CAN_SIGNATURE_MIC19:
+            can_app_msg_mic19_extractors_switch(msg);
+            break;
+        case CAN_SIGNATURE_MSWI19:
+            can_app_msg_mswi19_extractors_switch(msg);
+            break;
+        case CAN_SIGNATURE_MAC22:
+            can_app_msg_mac22_extractors_switch(msg);
+            break;
+        default: break;
     }
 }
 
@@ -261,6 +310,15 @@ inline void check_can(void)
         VERBOSE_MSG_CAN_APP(usart_send_string("Warning: mswi19 not connected. Using D from mic19."));
         can_app_checks_without_mswi19_msg = 0;
         mswi19_connected = 0;
+    }
+
+    if(can_app_checks_without_mac22_msg++ >= CAN_APP_CHECKS_WITHOUT_MAC22_MSG){
+        VERBOSE_MSG_CAN_APP(usart_send_string("Error: too many cycles without mac22 messages.\n"));
+        can_app_checks_without_mac22_msg = 0;
+        #ifdef SET_ERROR_WHEN_NO_STATE_MESSAGES_FROM_MAC22
+            error_flags.no_contactor = 1;
+            set_state_error();
+        #endif
     }
 
     if(can_check_message()){
